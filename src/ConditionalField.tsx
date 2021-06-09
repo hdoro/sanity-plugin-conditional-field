957b089e8d119233e885aedd2757931c288a1f54
Daniel Mierzwinski <dnl.mzw@gmail.com>
2021-06-09 21:03:14 +0200

    Added context
    Added context to fields allowing you not only to check for values at the document-level, but also for fields on the same level.

--- src/ConditionalField.tsx
+++ src/ConditionalField.tsx
@@ -14,6 +14,35 @@ class ConditionalField extends React.PureComponent<any> {
     }
   }
 
+  getContext(level = 1) {
+    // gets value path from withValuePath HOC, and applies path to document
+    // we remove the last 𝑥 elements from the valuePath
+
+    const valuePath = this.props.getValuePath()
+    const removeItems = -Math.abs(level)
+    return valuePath.length + removeItems <= 0
+      ? this.props.document
+      : valuePath.slice(0, removeItems).reduce((context, current) => {
+          // basic string path
+          if (typeof current === 'string') {
+            return context[current] || {}
+          }
+
+          // object path with key used on arrays
+          if (
+            typeof current === 'object' &&
+            Array.isArray(context) &&
+            current._key
+          ) {
+            return (
+              context.filter(
+                item => item._key && item._key === current._key
+              )[0] || {}
+            )
+          }
+        }, this.props.document)
+  }
+
   render() {
     const {
       document,
@@ -29,7 +58,7 @@ class ConditionalField extends React.PureComponent<any> {
       presence = [],
       compareValue,
     } = this.props
-    const shouldRenderField = type?.options?.condition
+    const shouldRenderField(document, this.getContext.bind(this))
     const renderField = shouldRenderField ? shouldRenderField(document) : true
 
     if (!renderField) {
